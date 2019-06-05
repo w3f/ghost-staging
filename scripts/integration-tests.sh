66050fd115bba45b62e52d51f2309057facdc5f6
Federico Gimenez <federico.gimenez@gmail.com>
2019-06-05 16:45:10 +0200

    initial import

---
+++ scripts/integration-tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source /scripts/common.sh
+source /scripts/bootstrap-helm.sh
+
+
+run_tests() {
+    echo Running tests...
+}
+
+teardown() {
+    helmfile delete --purge
+}
+
+main(){
+    if [ -z "$KEEP_GHOST_PREVIEW" ]; then
+        trap teardown EXIT
+    fi
+
+    run_tests
+}
+
+main
